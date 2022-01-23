b0b2ffbe66c56efb2e7927bdc9225db329f515cd
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-01-23 22:25:10 +0900

    Initial commit

---
+++ app/src/main/java/com/codewithkyo/webview/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.codewithkyo.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    private val loadingUrl = "http://10.0.2.2/tutorial"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.test_wv)
+        webView.webViewClient = object : WebViewClient(){}
+        webView.loadUrl(loadingUrl)
+    }
+}
